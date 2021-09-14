baccc0841b70dc74bf43cfc54a5893e527d3afa5
Tommaso <dippotommaso@gmail.com>
2021-09-14 15:46:18 +0200

    Added an example template file

---
+++ lv_cpp_examples/TemplateEx.cpp
@@ -0,0 +1,19 @@
+/*
+ * FlexEx.cpp
+ *
+ *  Created on: Aug 30, 2021
+ *      Author: fstuffdev
+ */
+
+#include "TemplateEx.h"
+using namespace lvglpp;
+
+namespace TemplateEx {
+
+/* Create Function */
+void Create() {
+	printf("Application created !!!\n");
+
+}
+
+}
---
+++ lv_cpp_examples/TemplateEx.h
@@ -0,0 +1,23 @@
+/*
+ * TemplateEx.h
+ *
+ *  Created on: Aug 30, 2021
+ *      Author: fstuffdev
+ */
+
+#ifndef TEMPLATEEX_H_
+#define TEMPLATEEX_H_
+
+#include "../lv_cpp/core/lvglpp.h"
+
+namespace TemplateEx {
+
+/* Milliseconds for application loop */
+const unsigned int ms = 5;
+
+/* Init the application */
+void Create();
+}
+
+
+#endif /* TEMPLATEEX_H_ */
